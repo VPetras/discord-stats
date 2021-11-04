3597277615c1111d1756ce44f9cf0c83d7b8b60d
Petrasek Vojtech <vojtech.petrasek@comap-control.com>
2021-11-04 22:29:56 +0100

    init upload

---
+++ app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+##################################################
+# Discord BOT
+# Title: Discord Bot for statistics of channels
+# Author: Vojtěch Petrásek 
+# Generated: 04/11/2021 20:55:01
+##################################################
+
+###
+# imports
+###
+
+import sys
+
+###
+# main
+###
+
+def main():
+    print("hello world")
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print('Exited with error: {}'.format(e))
+        sys.exit(1)
