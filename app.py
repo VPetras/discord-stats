fc3a41b16aeedbee1600b467c5f279b7573427cf
VPetras <vojtechpetrasek@gmail.com>
2021-11-10 20:15:14 +0100

    added graphs

--- app.py
+++ app.py
@@ -18,8 +18,72 @@ import discord
 from discord.ext import commands
 import asyncio
 import time
+import matplotlib.pyplot as plt
 
 bot = commands.Bot(command_prefix = "/")
+
+@bot.command("channels")
+async def channels(ctx):
+    channels = ctx.guild.text_channels
+    ch = []
+    for channel in channels:
+        ch.append(channel.name)
+    await ctx.send(ch)
+
+@bot.command("stats")
+async def stats(ctx):
+    msg = ctx.message.content.split(' ')
+    if len(msg) == 1:
+        await ctx.send('i need specific channel to make stats')
+    else:
+        channels = ctx.guild.text_channels
+        channel = None
+        for ch in channels:
+            if msg[1] == ch.name:
+                print('found')
+                channel = ch
+                break
+            print('a')
+        if id == None:
+            await ctx.send("Channel not found. Try /channels")
+        else:
+            await ctx.send("Channel found. Please wait. " + str(channel.id))
+            with open(channel.name + ".txt", "w", encoding='utf-8') as f:
+                users = {}
+                c = 0
+                async for message in channel.history(limit=999999999):
+                    #if message.author != bot.user:
+                        c +=1
+                        d = []
+                        if message.author.name not in users:
+                            users[message.author.name] = 0
+                        users[message.author.name] += 1
+                        d.append(message.author)
+                        d.append(message.content)
+                        d.append(message.reactions)
+
+                        f.write(str(d) +"\n")
+                await ctx.send(users)
+                await ctx.send(c)
+
+                user = []
+                count = []
+                coord = []
+                c = 0
+                for usr in users:
+                    user.append(usr)
+                    count.append(users[usr])
+                    coord.append(c)
+                    c +=1
+                
+                plt.bar(user,count, color=['blue'], width = 0.8)
+                plt.title(channel.name)
+                plt.savefig(channel.name + ".png")
+                with open(channel.name + ".png", 'rb') as f:
+                    graph = discord.File(f)
+                    await ctx.send(file=graph)
+                print(user,count)
+
 
 
 @bot.command("a")
@@ -42,7 +106,11 @@ async def copy(ctx):
 
 if __name__ == '__main__':
     try:
-        bot.run('')
+        with open('token') as t:
+            token = t.readline()
+            print(token)
+        t.close()
+        bot.run(token)
     except Exception as e:
         print('Exited with error: {}'.format(e))
         sys.exit(1)
